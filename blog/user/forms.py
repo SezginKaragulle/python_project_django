61ae107a5a335e0992049a065ebd7d4de4d91e1c
Sezgin Karagülle <sezgin.karagulle92@gmail.com>
2022-09-15 18:05:52 +0300

    this is my django project
    this is my django project

---
+++ blog/user/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label = "Kullanıcı Adı")
+    password = forms.CharField(label = "Parola",widget = forms.PasswordInput)
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(max_length = 50,label = "Kullanıcı Adı")
+    password = forms.CharField(max_length=20,label = "Parola",widget = forms.PasswordInput)
+    confirm = forms.CharField(max_length=20,label ="Parolayı Doğrula",widget = forms.PasswordInput)
